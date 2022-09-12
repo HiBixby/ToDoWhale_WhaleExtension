d15260d7735a3fb590f185ec1948b520be030786
HiBixby <201910789@sangmyung.kr>
2022-09-12 22:58:48 +0900

    ⚰️ Remove test codes related to the localstorage

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,3 +1,6 @@
+/*global whale*/
+/*global chrome*/
+/*global local*/
 import React, { useState } from "react";
 import moment from "moment";
 import "./EditPage.css";
@@ -6,13 +9,37 @@ const EditPage = (props) => {
   function onExit() {
     props.setMain(true);
     props.setEdit(false);
-    console.log(toDoInfo.id,toDo,link,time,isNotiOn);
-    localStorage.setItem("test", "1");
+    console.log(toDoInfo.id, date, toDo, link, time, isNotiOn);
+    // chrome.storage.local.set({ key: storage }, function () {
+    //   console.log("Value is set to " + storage);
+    // });
   }
   let toDoInfo = props.selectedToDo;
+  const [id, setId] = useState(toDoInfo.id);
+  const [date, setDate] = useState(toDoInfo.date);
   const [toDo, setToDo] = useState(toDoInfo.content);
   const [link, setLink] = useState(toDoInfo.link);
   const [time, setTime] = useState(toDoInfo.time);
+
+  const storage = [
+    {
+      id: 1,
+      date: new Date(),
+      time: "14:00",
+      content:
+        "아주아주아주아주아주매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우 긴 내용",
+      link: null,
+      noti: true,
+    },
+    {
+      id: 2,
+      date: new Date(),
+      time: "23:00",
+      content: "내용2",
+      link: "https://search.shopping.naver.com/book/catalog/33005838635?cat_id=50010881&frm=PBOKPRO&query=%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8&NaPm=ct%3Dl7vqq3xs%7Cci%3Df0bf3d0f13db5818474de3410294bdb3f4c913f1%7Ctr%3Dboknx%7Csn%3D95694%7Chk%3D113cde34ebdda1144796a6460a335e4b5f7eb701",
+      noti: false,
+    },
+  ];
 
   const [isNotiOn, setIsNotiOn] = useState(toDoInfo.noti);
   return (
@@ -20,7 +47,7 @@ const EditPage = (props) => {
       <div className="back" onClick={onExit}>
         <i className="fa-solid fa-chevron-left"></i>
       </div>
-      <div className="date">{moment(new Date()).format("YYYY.MM.DD")}</div>
+      <div className="date">{moment(date).format("YYYY.MM.DD")}</div>
       <div className="time-alarm">
         <div className="time">
           <input
