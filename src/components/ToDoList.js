12a62fe628fe65227bfb673cfb7020d3c59b9581
HiBixby <201910789@sangmyung.kr>
2022-09-12 22:57:10 +0900

    ✨ Change date to real "Date" tyle

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ToDoList.css";
 import moment from "moment";
 const ToDoList = (props) => {
-  const todos = [
+  const [todos,setTodos] = useState([
     {
-      id: 1,
-      date: "12",
+      id: 0,
+      date: new Date(),
       time: "14:00",
       content:
         "아주아주아주아주아주매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우 긴 내용",
@@ -13,18 +13,25 @@ const ToDoList = (props) => {
       noti: true,
     },
     {
-      id: 2,
-      date: "12",
+      id: 1,
+      date: new Date(),
       time: "23:00",
       content: "내용2",
       link: "https://search.shopping.naver.com/book/catalog/33005838635?cat_id=50010881&frm=PBOKPRO&query=%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8&NaPm=ct%3Dl7vqq3xs%7Cci%3Df0bf3d0f13db5818474de3410294bdb3f4c913f1%7Ctr%3Dboknx%7Csn%3D95694%7Chk%3D113cde34ebdda1144796a6460a335e4b5f7eb701",
       noti: false,
     },
-  ];
+  ]);
+  const isSameDate = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
   return (
     <>
       {todos.map((todo) =>
-        todo.date === moment(props.selectedDay).format("DD") ? (
+        isSameDate(todo.date, props.selectedDay) ? (
           <div key={todo.id}>
             <div className="todolist-time">{todo.time}</div>
             <div className="todolist-content">
