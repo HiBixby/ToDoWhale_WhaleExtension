cc8eb096d23eb81ad993f23dba813277ade9e167
HiBixby <201910789@sangmyung.kr>
2022-09-13 06:13:59 +0900

    ✨ Change noti status in main page

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -9,48 +9,56 @@ const ToDoList = (props) => {
       date1.getDate() === date2.getDate()
     );
   };
+  function editNoti(index){
+    const tmp = [...todos];
+    tmp[index].noti=!tmp[index].noti;
+    props.setTodos(tmp);
+  }
   const todos = props.todos;
   return (
     <>
-      {todos.sort((a, b) => a.time.localeCompare(b.time)).map((todo, index) =>
-        isSameDate(todo.date, props.selectedDay) ? (
-          <div key={index}>
-            <div className="todolist-time">
-              {todo.time == null ? "--:--" : todo.time}
-            </div>
-            <div className="todolist-content">
-              <div className="todolist-content-text">{todo.content}</div>
-              <div className="todolist-icons">
-                <i
-                  onClick={() => {
-                    props.setSelectedToDo(todo);
-                    props.goEditPage();
-                    props.setTodoIndex(index);
-                    console.log("인덱스", index);
-                  }}
-                  className="fa-solid fa-pen-to-square mx-3"
-                ></i>
-                <i
-                  className={
-                    (todo.noti ? "fa-solid" : "fa-regular") + " fa-bell"
-                  }
-                ></i>
+      {todos
+        .sort((a, b) => (a===null)-(b===null) || (''+a.time).localeCompare(b.time))
+        .map((todo, index) =>
+          isSameDate(todo.date, props.selectedDay) ? (
+            <div key={index}>
+              <div className="todolist-time">
+                {todo.time == null ? "--:--" : todo.time}
               </div>
+              <div className="todolist-content">
+                <div className="todolist-content-text">{todo.content}</div>
+                <div className="todolist-icons">
+                  <i
+                    onClick={() => {
+                      props.setSelectedToDo(todo);
+                      props.goEditPage();
+                      props.setTodoIndex(index);
+                      console.log("인덱스", index);
+                    }}
+                    className="fa-solid fa-pen-to-square mx-3"
+                  ></i>
+                  <i
+                    onClick={()=>{editNoti(index)}}
+                    className={
+                      (todo.noti ? "fa-solid" : "fa-regular") + " fa-bell"
+                    }
+                  ></i>
+                </div>
+              </div>
+              <div className="todolist-link">
+                {todo.link ? (
+                  <a href={todo.link} target="_blank">
+                    {todo.link}
+                  </a>
+                ) : (
+                  "No Link"
+                )}
+              </div>
+              <hr />
+              <br />
             </div>
-            <div className="todolist-link">
-              {todo.link ? (
-                <a href={todo.link} target="_blank">
-                  {todo.link}
-                </a>
-              ) : (
-                "No Link"
-              )}
-            </div>
-            <hr />
-            <br />
-          </div>
-        ) : null
-      )}
+          ) : null
+        )}
     </>
   );
 };
