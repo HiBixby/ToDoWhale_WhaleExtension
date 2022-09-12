c2a42d0a4fb17c5e8aaf510d4b3d5917b779a864
HiBixby <201910789@sangmyung.kr>
2022-09-12 22:58:04 +0900

    🤡 Add mock todo data into App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,48 @@ import MainPage from "./components/MainPage";
 import EditPage from "./components/EditPage";
 
 function App() {
-  const [isMainPage,setIsMainPage] = useState(true);
-  const [isEditPage,setEditPage]=useState(false);
-  const [selectedToDo,setSelectedToDo]=useState();
+  const [isMainPage, setIsMainPage] = useState(true);
+  const [isEditPage, setEditPage] = useState(false);
+  const [selectedToDo, setSelectedToDo] = useState();
+  const [todos, setTodos] = useState([
+    {
+      id: 0,
+      date: new Date(),
+      time: "14:00",
+      content:
+        "아주아주아주아주아주매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우매우 긴 내용",
+      link: null,
+      noti: true,
+    },
+    {
+      id: 1,
+      date: new Date(),
+      time: "23:00",
+      content: "내용2",
+      link: "https://search.shopping.naver.com/book/catalog/33005838635?cat_id=50010881&frm=PBOKPRO&query=%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8&NaPm=ct%3Dl7vqq3xs%7Cci%3Df0bf3d0f13db5818474de3410294bdb3f4c913f1%7Ctr%3Dboknx%7Csn%3D95694%7Chk%3D113cde34ebdda1144796a6460a335e4b5f7eb701",
+      noti: false,
+    },
+  ]);
   return (
     <div className="App antialiased">
-      {isMainPage? <MainPage setMain={setIsMainPage} setEdit={setEditPage} setSelectedToDo={setSelectedToDo}/>:null}
-      {isEditPage? <EditPage setMain={setIsMainPage} setEdit={setEditPage} selectedToDo={selectedToDo}/>:null}
+      {isMainPage ? (
+        <MainPage
+          setMain={setIsMainPage}
+          setEdit={setEditPage}
+          setSelectedToDo={setSelectedToDo}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ) : null}
+      {isEditPage ? (
+        <EditPage
+          setMain={setIsMainPage}
+          setEdit={setEditPage}
+          selectedToDo={selectedToDo}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ) : null}
     </div>
   );
 }
